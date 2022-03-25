5f050c89edc9af8d268bfc7dfea19de6147a0630
shruthipatil11 <shrutipatil1121@gmail.com>
2022-03-25 17:21:20 +0000

    feat : add grid and slider component

--- src/app/docs/docs.js
+++ src/app/docs/docs.js
@@ -5,21 +5,30 @@ const btn = document.getElementById("open-modal-btn");
 const modalClose = document.querySelector("#modal .close");
 
 btn.addEventListener("click", () => {
-    modal.classList.remove("display-none");
+  modal.classList.remove("display-none");
 });
 
 modalClose.addEventListener("click", () => {
   modal.classList.add("display-none");
 });
 
+let toastMessage = document.getElementById("toastMessage");
 
-  let toastMessage = document.getElementById("toastMessage");
-
- const openToast = () => {
+const openToast = () => {
   toastMessage.className = "display";
-  setTimeout(() => { toastMessage.className = toastMessage.className.replace("display", ""); }, 5000);
-}
+  setTimeout(() => {
+    toastMessage.className = toastMessage.className.replace("display", "");
+  }, 5000);
+};
 
 const closeToast = () => {
-    toastMessage.className = toastMessage.className.replace("display", "");
-}
+  toastMessage.className = toastMessage.className.replace("display", "");
+};
+
+let sliderRange = document.getElementById("range");
+let showSelectedRange = document.getElementsByClassName("showSelectedRange")[0];
+showSelectedRange.innerHTML = `Selected range is ${sliderRange.value} `;
+
+const getRangeValue = (e) => {
+  showSelectedRange.innerHTML = `Selected range is ${e.target.value} `;
+};
